afd7a1730746bb0c14f24b10181006f2db76bb0b
Isabel-Puertas9 <i.puertas.planes@gmail.com>
2024-02-16 19:10:24 +0100

    Subir los archivos a GitHub

---
+++ ProyectoEjemplo/src/main/java/com/mycompany/proyectoejemplo/A.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoejemplo;
+
+/**
+ *
+ * @author Profesor
+ */
+public class A 
+{
+    private int at1;
+
+    public A() 
+    {
+        this.at1 = 0;
+    }
+
+    public A(int at1) 
+    {
+        this.at1 = at1;
+    }
+
+    public int getAt1() 
+    {
+        return at1;
+    }
+
+    public void setAt1(int at1) 
+    {
+        this.at1 = at1;
+    }
+
+    @Override
+    public String toString() {
+        return "A{" + "at1=" + at1 + '}';
+    }
+    
+    
+    
+    
+    
+    
+    
+}
