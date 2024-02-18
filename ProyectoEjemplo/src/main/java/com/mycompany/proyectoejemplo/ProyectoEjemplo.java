32e76659b8a95b531921e209720cb44527abb8bf
jpdistancia <160269733+jpdistancia@users.noreply.github.com>
2024-02-18 11:52:06 +0100

    Update ProyectoEjemplo.java

--- ProyectoEjemplo/src/main/java/com/mycompany/proyectoejemplo/ProyectoEjemplo.java
+++ ProyectoEjemplo/src/main/java/com/mycompany/proyectoejemplo/ProyectoEjemplo.java
@@ -19,6 +19,8 @@ public class ProyectoEjemplo
         System.out.println("Hello World!");
         
         A objA = new A(5);
+
+        //Hecho desde otro lugar
         
         objA.toString();
         
